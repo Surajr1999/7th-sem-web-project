35f6b78e4a1fe1fee9776ce4940c2833cfb4ba44
surajr1999 <surajr1999@github.com>
2021-01-15 22:49:07 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+
+
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white"><b>&copy;2020,AMC Engineering College</b></p>
+        </div>
+        <!-- /.container -->
+    </footer>
